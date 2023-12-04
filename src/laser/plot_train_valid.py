0dd10382f17d19f1081800b2a24788f3e2c0b932
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2023-12-04 10:20:08 +0100

    add plot script to best checkpoint

--- src/laser/plot_train_valid.py
+++ src/laser/plot_train_valid.py
@@ -1,22 +1,24 @@
-import os, configargparse
+import os, argparse
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from plot import checkpoint_display_name
 
-input_dir = "/gpfsscratch/rech/rnh/udc54vm/experiments/robust-embeddings/laser/experiment_026_valid/scores"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input-dir", help="path to directory to read validation scores", type=str)
+    args = parser.parse_args()
 
-all_scores = pd.read_csv(os.path.join(input_dir, "all_scores" + ".csv" ))
-#all_scores.drop(all_scores[all_scores['model'] == 'roberta-init'].index, inplace=True)
-#all_scores.drop(all_scores[all_scores['model'] == 'c-roberta-init'].index, inplace=True)
+    all_scores = pd.read_csv(os.path.join(args.input_dir, "all_scores" + ".csv" ))
+    #all_scores.drop(all_scores[all_scores['model'].endswith('init')].index, inplace=True)
 
-def plot_loss(lossname):
-    plt.clf()
-    g = sns.lineplot(all_scores[all_scores['steps'] < 700000], x='steps', y=lossname, hue='model')
-    g.set_xticklabels([checkpoint_display_name(str(int(c))) for c in g.get_xticks()])
-    plt.savefig(os.path.join(input_dir, lossname + ".pdf"), format="pdf")
+    def plot_loss(lossname):
+        plt.clf()
+        g = sns.lineplot(all_scores[all_scores['steps'] < 700000], x='steps', y=lossname, hue='model')
+        g.set_xticklabels([checkpoint_display_name(str(int(c))) for c in g.get_xticks()])
+        plt.savefig(os.path.join(args.input_dir, lossname + ".pdf"), format="pdf")
 
-for loss in ["loss_std_gold", "loss_ugc_gold", "valid_distil_loss"]:
-    plot_loss(loss)
+    for loss in ["loss_std_gold", "loss_ugc_gold", "valid_distil_loss"]:
+        plot_loss(loss)
 
 
