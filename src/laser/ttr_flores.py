bdf3ec4d057068876b2c3b10a4d104ee41013fab
lnishimw <lydia.nishimwe@inria.fr>
2023-12-19 18:48:19 +0100

    fix verbose printing

--- src/laser/ttr_flores.py
+++ src/laser/ttr_flores.py
@@ -14,12 +14,13 @@ if __name__ == "__main__":
     gold_file = "eng_Latn"
 
     print("Processing", corpus)
-    for seed in range(100,110):    
+    for seed in range(100,110):   
+        print("\tseed", seed)
         for corpus_part in corpus_parts:
             results = []
-            files = [ file for file in os.scandir(os.path.join(args.input_dir, seed, corpus_part)) if file.name.endswith(f"{corpus_part}.tok") ]
+            files = [ file for file in os.scandir(os.path.join(args.input_dir, str(seed), corpus_part)) if file.name.endswith(f"{corpus_part}.tok") ]
             for file in files:
-                print(file.name)
+                print(f"\t{file.name}")
                 with open(file.path, "r") as f:
                     doc = f.read().replace('\n',' ')
 
@@ -29,14 +30,14 @@ if __name__ == "__main__":
                 results.append({"file": file.name[:-4], "tokens": len(tokens), "types": len(types), "ttr": ttr})
 
             results_df = pd.DataFrame(results)
-            results_df.to_csv(os.path.join(args.input_dir, seed, corpus_part, _file(metric)))
+            results_df.to_csv(os.path.join(args.input_dir, str(seed), corpus_part, _file(metric)))
     
-    print("Aggregating results...")
+    print("\tAggregating results...")
     for corpus_part in corpus_parts:
         output_file = os.path.join(args.input_dir, corpus_part + '-' + _file(metric))
         all_data = pd.DataFrame(columns=["file", "types", "tokens", "ttr"])
         for seed in range(100,110):
-            input_file = os.path.join(args.input_dir, seed, corpus_part, _file(metric))
+            input_file = os.path.join(args.input_dir, str(seed), corpus_part, _file(metric))
             data = pd.read_csv(input_file)
             all_data = pd.concat([all_data, data[all_data.columns]], ignore_index=True)
         all_data.to_csv(output_file)
@@ -45,7 +46,7 @@ if __name__ == "__main__":
             average_output_file = os.path.join(args.input_dir, corpus_part + '-avg-' + _file(metric))
             average_data = pd.DataFrame(columns=["file", _name(metric), "p-value", "not significant", "significant", "very significant", "highly significant"])
             average_data[["file", _name(metric)]] = all_data[["file", _name(metric)]].groupby("file").mean().reset_index()
-            target_mean = all_data[all_data["file"]==gold_file+"."+corpus_part][_name(metric)].mean()
+            target_mean = all_data[all_data["file"]==f"{gold_file}.{corpus_part}"][_name(metric)].mean()
             p_values = [ ttest_1samp(all_data[all_data["file"]==file][_name(metric)], target_mean)[1] for file in average_data["file"] ]
             average_data["p-value"] = p_values
             average_data["not significant"] = average_data["p-value"] >= 0.05
