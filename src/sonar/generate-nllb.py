d9813d07e9a90968369be228612d49c34980deb2
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2024-07-25 18:23:27 +0200

    generate nllb with transformers

--- src/sonar/generate-nllb.py
+++ src/sonar/generate-nllb.py
@@ -1,87 +1,5 @@
 import os, argparse
-from fairseq2.models.nllb import (
-    create_nllb_model, 
-    load_nllb_config, 
-    load_nllb_tokenizer,
-    NllbTokenizer
-)
-from fairseq2.generation import BeamSearchSeq2SeqGenerator, TextTranslator
-from fairseq2.models.transformer import TransformerModel
-from typing import Iterable, List, Optional, Sequence, Union
-from fairseq2.typing import Device
-import torch
-from pathlib import Path
-from fairseq2.data import StringLike
-from fairseq2.data.cstring import CString
-from fairseq2.data.data_pipeline import read_sequence
-from fairseq2.data.text import TextTokenizer, read_text
-from sonar.inference_pipelines.utils import add_progress_bar
-
-class NLLBTranslationPipeline(torch.nn.Module):
-    model: TransformerModel
-    tokenizer: NllbTokenizer
-
-    def __init__(
-        self,
-        model: Union[str, TransformerModel],
-        tokenizer: Union[str, NllbTokenizer],
-        device: Device = torch.device("cuda")
-    ) -> None:
-        """
-        Args:
-            model (Union[str, TransformerModel]): either card name or model object
-            decoder (Union[str, NllbTokenizer]): either card name or model object
-            device (device, optional): . Defaults to GPU.
-        """
-        super().__init__()
-        if isinstance(model, str):
-            config = load_nllb_config(model)
-            model = create_nllb_model(config, dtype=torch.float32)
-
-        if isinstance(tokenizer, str):
-            tokenizer = load_nllb_tokenizer(tokenizer, progress=False)
-
-        self.model = model.to(device).eval()   
-        self.tokenizer = tokenizer
-    
-    @torch.inference_mode()
-    def predict(
-        self,
-        input: Union[Path, Sequence[str]],
-        source_lang: str,
-        target_lang: str,
-        batch_size: int = 5,
-        progress_bar: bool = False,
-        **generator_kwargs,
-    ) -> List[str]:
-        generator = BeamSearchSeq2SeqGenerator(self.model, **generator_kwargs)
-        translator = TextTranslator(
-            generator,
-            tokenizer=self.tokenizer,
-            source_lang=source_lang,
-            target_lang=target_lang,
-        )
-
-        def _do_translate(src_texts: List[StringLike]) -> List[StringLike]:
-            texts, _ = translator.batch_translate(src_texts)
-            return texts
-
-        pipeline: Iterable = (
-            (
-                read_text(input)
-                if isinstance(input, (str, Path))
-                else read_sequence(input)
-            )
-            .bucket(batch_size)
-            .map(_do_translate)
-            .and_return()
-        )
-        if progress_bar:
-            pipeline = add_progress_bar(pipeline, inputs=input, batch_size=batch_size)
-
-        results: List[List[CString]] = list(iter(pipeline))
-        return [str(x) for y in results for x in y]
-
+from transformers import AutoModelForSeq2SeqLM, NllbTokenizer
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -96,10 +14,15 @@ if __name__ == "__main__":
     file_name = os.path.basename(args.input_file)
     output_file = os.path.join(args.output_dir, f"{file_name}.out")
 
-    print("Loading translation pipeline...")
-    translator = NLLBTranslationPipeline(model=args.model_name,
-                                        tokenizer="nllb-200",
-                                        device=torch.device("cuda")
+    print("Loading translation model and tokenizer...")
+    tokenizer = NllbTokenizer.from_pretrained(
+        args.model_name,
+        src_lang=args.src_lang,
+        tgt_lang=args.tgt_lang
+    )
+    model = AutoModelForSeq2SeqLM.from_pretrained(
+        args.model_name,
+        device_map="cuda"
     )
     
     with open(args.input_file) as f:
@@ -107,12 +30,13 @@ if __name__ == "__main__":
     sentences = [line.strip() for line in data]
 
     print("Generating outputs...")
-    outputs = translator.predict(sentences, 
-        source_lang=args.src_lang,
-        target_lang=args.tgt_lang,
-        progress_bar=True,
-        batch_size=32
+    inputs = tokenizer(sentences, return_tensors="pt", padding=True).to(model.device)
+    output_tokens = model.generate(
+        **inputs,
+        forced_bos_token_id=tokenizer.convert_tokens_to_ids(tokenizer.tgt_lang),
+        max_length=512
     )
+    outputs = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)
 
     with open(output_file, "w") as f:
         for output in outputs:
