bb98fdf17cbccf5a09e56dc5a6d8da5c4f7eb73a
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2024-07-27 01:52:04 +0200

    print score files

--- src/sonar/aggregate-artificial.py
+++ src/sonar/aggregate-artificial.py
@@ -39,8 +39,8 @@ if __name__ == "__main__":
                     model_output_dir = os.path.join(args.input_dir, "outputs", model, args.corpus, lang_pair, str(seed), str(proba))
                     if os.path.isdir(model_output_dir):
                         scores_files = [ f.path for f in os.scandir(model_output_dir) if f.name.endswith(SCORE_FILE_SUFFIX)]
+                        print(score_files)
                         for score_file in scores_files:
-                            print(score_file)
                             file_name = os.path.basename(score_file).removesuffix(SCORE_FILE_SUFFIX)
                             column_name = "__".join([args.corpus, lang_pair, file_name])
                             with open(score_file) as f:
