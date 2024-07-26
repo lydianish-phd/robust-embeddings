484082588bc7201d094417df2d437ac59313f331
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2024-07-26 16:17:58 +0200

    script to aggregate scores

--- src/sonar/aggregate.py
+++ src/sonar/aggregate.py
@@ -1,11 +1,47 @@
 import os, argparse, json
 import pandas as pd
 
+SCORE_FILE_SUFFIX = ".out.json"
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input-file", type=str)
-    parser.add_argument("-s", "--src-lang", type=str)
-    parser.add_argument("-t", "--tgt-lang", type=str)
-    parser.add_argument("-o", "--output-dir", type=str)
-    parser.add_argument("-m", "--model-name", type=str)
+    parser.add_argument("-i", "--input-dir", help="path to experiment directory", type=str)
+    parser.add_argument("-t", "--table-name", type=str)
+    parser.add_argument("-c", "--corpora", type=str, nargs="+")
+    parser.add_argument("-l", "--lang-pais", type=str, nargs="+")
+    parser.add_argument("-m", "--models", type=str, nargs="+")
     args = parser.parse_args()
+
+    bleu_scores = {
+        "model": arg.models
+    }
+    comet_scores = {
+        "model": arg.models
+    }
+
+    for model in args.models:
+        for corpus in args.corpora:
+            for lang_pair in args.lang_pairs:
+                model_output_dir = os.path.join(args.input_dir, "outputs", model, corpus, lang_pair)
+                if os.path.isdir(model_output_dir)
+                    scores_files = [ f.path for f in os.scandir(model_output_dir) if f.name.endswith(SCORE_FILE_SUFFIX)]
+                    for score_file in scores_files:
+                        file_name = os.path.basename(score_file).removesuffix(SCORE_FILE_SUFFIX)
+                        column_name = "__".join([corpus, lang_pair, file_name])
+                        with open(score_file) as f:
+                            scores = json.load(f)
+                        bleu_scores[column_name].append(round(scores["bleu"], 2))
+                        comet_scores[column_name].append(round(scores["comet"], 3))
+    
+    print("Writing aggregated score files...")
+    scores_dir = os.path.join(args.input_dir, "scores")
+    os.makedirs(args.scores_dir, exist_ok=True)
+    bleu_score_file = os.path.join(scores_dir, f"bleu_{args.table_name}.csv")
+    comet_score_file = os.path.join(scores_dir, f"comet_{args.table_name}.csv")
+
+    bleu_scores_df = pd.DataFrame.from_dict(bleu_scores)
+    comet_scores_df = pd.DataFrame.from_dict(comet_scores)
+
+    bleu_scores_df.to_csv(bleu_score_file)
+    comet_scores_df.to_csv(comet_score_file)
+
