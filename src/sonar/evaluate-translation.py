abaa4201f0dabfa727399b4ba6b64fca9d54ecf7
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2024-07-26 21:53:10 +0200

    rewrite evaluate code to process multiple systems at once

--- src/sonar/evaluate-translation.py
+++ src/sonar/evaluate-translation.py
@@ -5,34 +5,34 @@ from comet import download_model, load_from_checkpoint
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--src-file", type=str)
-    parser.add_argument("--sys-file", type=str)
+    parser.add_argument("--sys-files", type=str, nargs="+")
     parser.add_argument("--ref-file", type=str)
     args = parser.parse_args()
-
-    print("Loading data...")
-    with open (args.src_file) as f:
-        src_data = [ line.strip() for line in f.readlines() ]
-
-    with open (args.sys_file) as f:
-        sys_data = [ line.strip() for line in f.readlines() ]
-
-    with open (args.ref_file) as f:
-        ref_data = [ line.strip() for line in f.readlines() ]
-
-    data = [{"src": src, "mt": mt, "ref": ref} for src, mt, ref in zip(src_data, sys_data, ref_data)]
 
     print("Loading metric models...")
     bleu_model = BLEU()
     comet_model_path = download_model("Unbabel/wmt22-comet-da")
     comet_model = load_from_checkpoint(comet_model_path)
     
-    print("Computing BLEU and COMET scores...")
-    scores = {
-        "bleu": bleu_model.corpus_score(sys_data, [ref_data]).score,
-        "comet": comet_model.predict(data, batch_size=32, gpus=1)[1]
-    }
+    print("Loading data...")
+    with open (args.src_file) as f:
+        src_data = [ line.strip() for line in f.readlines() ]
 
-    output_file = f"{args.sys_file}.json"
-    with open(output_file, 'w') as f:
-        json.dump(scores, f)
+    with open (args.ref_file) as f:
+        ref_data = [ line.strip() for line in f.readlines() ]
+
+    print("Computing BLEU and COMET scores...")
+    for sys_file in args.sys_files:
+        with open (sys_file) as f:
+            sys_data = [ line.strip() for line in f.readlines() ]
+        data = [{"src": src, "mt": mt, "ref": ref} for src, mt, ref in zip(src_data, sys_data, ref_data)]
+   
+        scores = {
+            "bleu": bleu_model.corpus_score(sys_data, [ref_data]).score,
+            "comet": comet_model.predict(data, batch_size=32, gpus=1)[1]
+        }
+
+        output_file = f"{args.sys_file}.json"
+        with open(output_file, 'w') as f:
+            json.dump(scores, f)
 
