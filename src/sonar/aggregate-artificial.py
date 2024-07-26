3e93c9d3f3161884e7421268aaf0550749f443da
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2024-07-26 22:39:38 +0200

    rewrite evaluation loop for artificial data

--- src/sonar/aggregate-artificial.py
+++ src/sonar/aggregate-artificial.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input-dir", help="path to experiment directory", type=str)
     parser.add_argument("-t", "--table-name", type=str)
-    parser.add_argument("-c", "--corpora", type=str, nargs="+")
+    parser.add_argument("-c", "--corpus", type=str)
     parser.add_argument("-l", "--lang-pairs", type=str, nargs="+")
     parser.add_argument("-m", "--models", type=str, nargs="+")
     parser.add_argument("-s", "--seeds", type=int, nargs="+")
@@ -28,29 +28,28 @@ if __name__ == "__main__":
     print(f"Aggregating {args.table_name} scores...")
     for seed in args.seeds:
         for proba in args.probas:
-            for corpus in args.corpora:
-                for lang_pair in args.lang_pairs:
-                    for model in args.models:
-                        model_output_dir = os.path.join(args.input_dir, "outputs", model, corpus, lang_pair, str(seed), str(proba))
-                        if os.path.isdir(model_output_dir):
-                            scores_files = [ f.path for f in os.scandir(model_output_dir) if f.name.endswith(SCORE_FILE_SUFFIX)]
-                            for score_file in scores_files:
-                                file_name = os.path.basename(score_file).removesuffix(SCORE_FILE_SUFFIX)
-                                column_name = "__".join([corpus, lang_pair, file_name])
-                                with open(score_file) as f:
-                                    scores = json.load(f)
-                                if column_name in bleu_scores:
-                                    bleu_scores[column_name].append(scores["bleu"])
-                                else:
-                                    bleu_scores[column_name] = [scores["bleu"]]
-                                if column_name in comet_scores:
-                                    comet_scores[column_name].append(scores["comet"])
-                                else:
-                                    comet_scores[column_name] = [scores["comet"]]
-                                bleu_scores["seed"].append(seed)
-                                comet_scores["seed"].append(seed)
-                                bleu_scores["proba"].append(proba)
-                                comet_scores["proba"].append(proba)
+            for lang_pair in args.lang_pairs:
+                for model in args.models:
+                    model_output_dir = os.path.join(args.input_dir, "outputs", model, args.corpus, lang_pair, str(seed), str(proba))
+                    if os.path.isdir(model_output_dir):
+                        scores_files = [ f.path for f in os.scandir(model_output_dir) if f.name.endswith(SCORE_FILE_SUFFIX)]
+                        for score_file in scores_files:
+                            file_name = os.path.basename(score_file).removesuffix(SCORE_FILE_SUFFIX)
+                            column_name = "__".join([args.corpus, lang_pair, file_name])
+                            with open(score_file) as f:
+                                scores = json.load(f)
+                            if column_name in bleu_scores:
+                                bleu_scores[column_name].append(scores["bleu"])
+                            else:
+                                bleu_scores[column_name] = [scores["bleu"]]
+                            if column_name in comet_scores:
+                                comet_scores[column_name].append(scores["comet"])
+                            else:
+                                comet_scores[column_name] = [scores["comet"]]
+                            bleu_scores["seed"].append(seed)
+                            comet_scores["seed"].append(seed)
+                            bleu_scores["proba"].append(proba)
+                            comet_scores["proba"].append(proba)
     
     print(f"Writing aggregated score files...")
     scores_dir = os.path.join(args.input_dir, "scores")
---
+++ src/sonar/evaluate-artificial.py
@@ -0,0 +1,51 @@
+import os, argparse, json
+from sacrebleu.metrics import BLEU
+from comet import download_model, load_from_checkpoint
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input-dir", help="path to experiment directory", type=str)
+    parser.add_argument("-c", "--corpus", type=str)
+    parser.add_argument("-l", "--lang-pair", type=str)
+    parser.add_argument("--src-dir", type=str)
+    parser.add_argument("--src-file-name", type=str)
+    parser.add_argument("--sys-file-name", type=str)
+    parser.add_argument("--ref-file", type=str)
+    parser.add_argument("-m", "--models", type=str, nargs="+")
+    parser.add_argument("-s", "--seeds", type=int, nargs="+")
+    parser.add_argument("-p", "--probas", type=float, nargs="+")
+    args = parser.parse_args()
+
+    print("Loading metric models...")
+    bleu_model = BLEU()
+    comet_model_path = download_model("Unbabel/wmt22-comet-da")
+    comet_model = load_from_checkpoint(comet_model_path)
+    
+    print("Loading data...")
+
+    with open (args.ref_file) as f:
+        ref_data = [ line.strip() for line in f.readlines() ]
+
+    print("Computing BLEU and COMET scores...")
+    for seed in args.seeds:
+        for proba in args.probas:
+            src_file = os.path.join(args.src_dir, str(seed), str(proba), "ugc", args.src_file_name)
+            for model in args.models:
+                model_output_dir = os.path.join(args.input_dir, "outputs", model, args.corpus, args.lang_pair, str(seed), str(proba))
+                sys_file = os.path.join(model_output_dir, args.sys_file_name)
+                
+                with open (args.src_file) as f:
+                    src_data = [ line.strip() for line in f.readlines() ]
+                with open (sys_file) as f:
+                    sys_data = [ line.strip() for line in f.readlines() ]
+                data = [{"src": src, "mt": mt, "ref": ref} for src, mt, ref in zip(src_data, sys_data, ref_data)]
+        
+                scores = {
+                    "bleu": bleu_model.corpus_score(sys_data, [ref_data]).score,
+                    "comet": comet_model.predict(data, batch_size=32, gpus=1)[1]
+                }
+
+                output_file = f"{sys_file}.json"
+                with open(output_file, 'w') as f:
+                    json.dump(scores, f)
+
