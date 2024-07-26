4a9ad23467a82eb3e8331097074eb1675e2fec6f
lnishimw <lydia.nishimwe@inria.fr>
2024-07-26 20:03:37 +0200

    define multilingual delta and average

--- src/sonar/aggregate.py
+++ src/sonar/aggregate.py
@@ -3,11 +3,24 @@ import pandas as pd
 
 SCORE_FILE_SUFFIX = ".out.json"
 
-def multilingual_average(data):
+def multilingual_average(scores):
+    unique_files_names = set()
+    for name in scores.columns:
+        corpus, _ , name = name.split("__")
+        unique_files_names.add((corpus, name))
+    for corpus, name in unique_files_names:
+        column_names = [col for col in scores.columns if name in col]
+        avg_column_name = "__".join(["avg", corpus, name])
+        scores[avg_column_name] = scores[column_names].mean(axis=1)
+    return scores
 
-
-def multilingual_delta(data, lang_pairs, corpus="rocsmt", ugc_file_name="raw.en.test", std_file_name="norm.en.test"):
-
+def multilingual_delta(scores, lang_pairs, ugc_file_name="raw.en.test", std_file_name="norm.en.test"):
+    column_names = [col for col in scores.columns if ugc_file_name in col]
+    for ugc_col in column_names:
+        col_name_prefix = ugc_col.removesuffix(ugc_file_name).strip("__")
+        std_col = "__".join([col_name_prefix, std_file_name])
+        delta_column_name = "__".join(["delta", col_name_prefix])
+        scores[delta_column_name] = scores[ugc_col] - scores[std_col]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -39,13 +52,14 @@ if __name__ == "__main__":
                         with open(score_file) as f:
                             scores = json.load(f)
                         if column_name in bleu_scores:
-                            bleu_scores[column_name].append(round(scores["bleu"], 2))
+                            bleu_scores[column_name].append(scores["bleu"])
                         else:
-                            bleu_scores[column_name] = [round(scores["bleu"], 2)]
+                            bleu_scores[column_name] = [scores["bleu"]]
                         if column_name in comet_scores:
-                            comet_scores[column_name].append(round(scores["comet"], 3))
+                            comet_scores[column_name].append(scores["comet"])
                         else:
-                            comet_scores[column_name] = [round(scores["comet"], 3)]
+                            comet_scores[column_name] = [scores["comet"]]
+
     
     print("Writing aggregated score files...")
     scores_dir = os.path.join(args.input_dir, "scores")
@@ -55,6 +69,9 @@ if __name__ == "__main__":
 
     bleu_scores_df = pd.DataFrame.from_dict(bleu_scores).set_index("model")
     comet_scores_df = pd.DataFrame.from_dict(comet_scores).set_index("model")
+
+    
+
 
     bleu_scores_df.to_csv(bleu_score_file)
     comet_scores_df.to_csv(comet_score_file)
