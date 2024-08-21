3339243139f5aab376737116b7908268c39cb011
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2024-08-21 16:42:14 +0200

    count samples by batches

--- src/sonar/steps.py
+++ src/sonar/steps.py
@@ -17,28 +17,24 @@ bilingual_data_dir = os.path.join(os.environ["DATASETS"], "rosonar/bilingual/con
 monolingual_data_dir = os.path.join(os.environ["DATASETS"], "rosonar/monolingual/concatenated")
 
 data_en_fr_files = {
-    "train": f"{bilingual_data_dir}/eng-fra/train.eng_Latn-fra_Latn_chunks/train.eng_Latn-fra_Latn-*.jsonl",
-    "valid": f"{bilingual_data_dir}/eng-fra/valid.eng_Latn-fra_Latn_chunks/valid.eng_Latn-fra_Latn-*.jsonl"
+    "train": f"{bilingual_data_dir}/eng-fra/train.eng_Latn-fra_Latn_chunks/train.eng_Latn-fra_Latn-*.jsonl"
 }
 data_en_fr = load_dataset("json", data_files=data_en_fr_files, streaming=True)
 data_en_fr = data_en_fr.shuffle(seed=seed, buffer_size=10_000)
 
 data_fr_files = {
-    "train": f"{monolingual_data_dir}/fra/train.fra_Latn-fra_Latn_chunks/train.fra_Latn-fra_Latn-*.jsonl",
-    "valid": f"{monolingual_data_dir}/fra/valid.fra_Latn-fra_Latn_chunks/valid.fra_Latn-fra_Latn-*.jsonl"
+    "train": f"{monolingual_data_dir}/fra/train.fra_Latn-fra_Latn_chunks/train.fra_Latn-fra_Latn-*.jsonl"
 }
 data_fr = load_dataset("json", data_files=data_fr_files, streaming=True)
 data_fr = data_fr.shuffle(seed=seed, buffer_size=10_000)
 
 data_en_files = {
-    "train": f"{monolingual_data_dir}/eng/train.eng_Latn-eng_Latn_chunks/train.eng_Latn-eng_Latn-*.jsonl",
-    "valid": f"{monolingual_data_dir}/eng/valid.eng_Latn-eng_Latn_chunks/valid.eng_Latn-eng_Latn-*.jsonl"
+    "train": f"{monolingual_data_dir}/eng/train.eng_Latn-eng_Latn_chunks/train.eng_Latn-eng_Latn-*.jsonl"
 }
 data_en = load_dataset("json", data_files=data_en_files, streaming=True)
 data_en = data_en.shuffle(seed=seed, buffer_size=10_000)
 
 all_train_data = interleave_datasets([data_en_fr["train"], data_fr["train"], data_en["train"]], probabilities=[fraction_en_fr/8, fraction_fr/8, fraction_en/8], seed=seed)
-all_valid_data = interleave_datasets([data_en_fr["valid"], data_fr["valid"], data_en["valid"]], seed=seed)
 
 data_loader = DataLoader(all_train_data, batch_size=32, num_workers=4)
 
