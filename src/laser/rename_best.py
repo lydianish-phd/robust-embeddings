d7832f52bc8fc1a921a455e67a440c650de1705c
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2024-08-30 00:39:29 +0200

    add rolaser v2 models to rename best

--- src/laser/rename_best.py
+++ src/laser/rename_best.py
@@ -1,6 +1,38 @@
 import os,argparse, shutil
 import pandas as pd
+
+def get_experiment_dir(model_name, base_dir):
     
+    experiment_dir_dict = {
+        "roberta-maxpool": base_dir,
+        "roberta-maxpool-init": f"{base_dir}b",
+        "roberta-meanpool": f"{base_dir}c",
+        "roberta-meanpool-init": f"{base_dir}d",
+        "c-roberta-maxpool": f"{base_dir}e",
+        "c-roberta-maxpool-init": f"{base_dir}f",
+        "c-roberta-meanpool": f"{base_dir}g",
+        "c-roberta-meanpool-init": f"{base_dir}h",
+        "roberta-cls": f"{base_dir}i",
+        "roberta-cls-init": f"{base_dir}j",
+        "c-roberta-cls": f"{base_dir}k",
+        "c-roberta-cls-init": f"{base_dir}l",
+        "roberta-maxpool-init-0.1": base_dir,
+        "roberta-maxpool-init-0.2": f"{base_dir}b",
+        "roberta-maxpool-init-0.3": f"{base_dir}c",
+        "roberta-maxpool-init-0.4": f"{base_dir}d",
+        "roberta-maxpool-init-0.5": f"{base_dir}e",
+        "roberta-maxpool-init-0": f"{base_dir}f",
+        "roberta-maxpool-init-0.35": f"{base_dir}g",
+        "roberta-maxpool-init-0.45": f"{base_dir}h",
+        "roberta-maxpool-init-0.6": f"{base_dir}i",
+        "RoLASER-0.1": base_dir,
+        "RoLASER-0.2": f"{base_dir}b",
+        "RoLASER-0.3": f"{base_dir}c",
+    }
+    
+    return experiment_dir_dict[model_name]
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--basedir", help="path to experiment directory", type=str)
@@ -10,51 +42,7 @@ if __name__ == "__main__":
     best_scores = pd.read_csv(os.path.join(f"{args.basedir}_valid", str(args.seed), "scores", "idx_best_valid_distil_loss.csv"))
     
     for _, row in best_scores.iterrows():
-        if row['model'] == "roberta-maxpool":
-            experiment_dir = args.basedir
-        elif row['model'] == "roberta-maxpool-init":
-            experiment_dir = f"{args.basedir}b"
-        elif row['model'] == "roberta-meanpool":
-            experiment_dir = f"{args.basedir}c"
-        elif row['model'] == "roberta-meanpool-init":
-            experiment_dir = f"{args.basedir}d"
-        elif row['model'] == "c-roberta-maxpool":
-            experiment_dir = f"{args.basedir}e"
-        elif row['model'] == "c-roberta-maxpool-init":
-            experiment_dir = f"{args.basedir}f"
-        elif row['model'] == "c-roberta-meanpool":
-            experiment_dir = f"{args.basedir}g"
-        elif row['model'] == "c-roberta-meanpool-init":
-            experiment_dir = f"{args.basedir}h"
-        elif row['model'] == "roberta-cls":
-            experiment_dir = f"{args.basedir}i"
-        elif row['model'] == "roberta-cls-init":
-            experiment_dir = f"{args.basedir}j"
-        elif row['model'] == "c-roberta-cls":
-            experiment_dir = f"{args.basedir}k"
-        elif row['model'] == "c-roberta-cls-init":
-            experiment_dir = f"{args.basedir}l"
-        elif row['model'] == "roberta-maxpool-init-0.1":
-            experiment_dir = args.basedir
-        elif row['model'] == "roberta-maxpool-init-0.2":
-            experiment_dir = f"{args.basedir}b"
-        elif row['model'] == "roberta-maxpool-init-0.3":
-            experiment_dir = f"{args.basedir}c"
-        elif row['model'] == "roberta-maxpool-init-0.4":
-            experiment_dir = f"{args.basedir}d"
-        elif row['model'] == "roberta-maxpool-init-0.5":
-            experiment_dir = f"{args.basedir}e"
-        elif row['model'] == "roberta-maxpool-init-0":
-            experiment_dir = f"{args.basedir}f"
-        elif row['model'] == "roberta-maxpool-init-0.35":
-            experiment_dir = f"{args.basedir}g"
-        elif row['model'] == "roberta-maxpool-init-0.45":
-            experiment_dir = f"{args.basedir}h"
-        elif row['model'] == "roberta-maxpool-init-0.6":
-            experiment_dir = f"{args.basedir}i"
-        else:
-            raise ValueError("Invalid input: Model name unknown")
-
+        experiment_dir = get_experiment_dir(row['model'], args.basedir)
         best_checkpoint = os.path.join(experiment_dir, "models", f"checkpoint_{row['epoch']}_{row['steps']}.pt")
         if not os.path.exists(best_checkpoint):
             best_checkpoint = os.path.join(experiment_dir, "models", f"checkpoint{row['epoch']}.pt")
