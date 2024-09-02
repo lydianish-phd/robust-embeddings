38efff6105e7ecd88328629b682c312694f430f8
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2024-09-02 11:33:07 +0200

    add metric to arg file

--- src/laser/plot-proba.py
+++ src/laser/plot-proba.py
@@ -10,17 +10,17 @@ COLORS = plt.cm.tab10.colors
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input-file", help="path to artificial scores csv file", type=str)
+    parser.add_argument("-m", "--metric", help="metric to plot", type=str)
     args = parser.parse_args()
 
-    output_dir , file_name = os.path.split(args.input_file)
-    metric = file_name.split("_")[0].split("-")[-1]
-    output_file = os.path.join(output_dir, f"{metric}_noise_proba_plot.pdf")
+    output_dir , _ = os.path.split(args.input_file)
+    output_file = os.path.join(output_dir, f"{args.metric}_noise_proba_plot.pdf")
     scores = pd.read_csv(args.input_file)
 
     print("Creating plot...")
 
-    g = sns.lineplot(data=scores, x="proba", y=_name(metric), hue="model", style="model", markers=True, dashes=False)
-    g.set(ylabel=f"{_display_name(metric)} score", xlabel="Probability of artificial UGC", xticks=[0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
+    g = sns.lineplot(data=scores, x="proba", y=_name(args.metric), hue="model", style="model", markers=True, dashes=False)
+    g.set(ylabel=f"{_display_name(args.metric)} score", xlabel="Probability of artificial UGC", xticks=[0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
     _, labels = g.get_legend_handles_labels()
     g.grid()
     plt.tight_layout()
