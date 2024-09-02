e52d5546f3cc38997aa2e8aa362b28fa0fea2d16
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2024-09-02 11:29:26 +0200

    fix bug

--- src/laser/plot-proba.py
+++ src/laser/plot-proba.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     output_dir , file_name = os.path.split(args.input_file)
-    metric = file_name.split("_")[0]
+    metric = file_name.split("_")[0].split("-")[-1]
     output_file = os.path.join(output_dir, f"{metric}_noise_proba_plot.pdf")
-    artificial_scores = pd.read_csv(args.input_file)
+    scores = pd.read_csv(args.input_file)
 
     print("Creating plot...")
 
-    g = sns.lineplot(data=artificial_scores, x="proba", y=_name(metric), hue="model", style="model", markers=True, dashes=False)
+    g = sns.lineplot(data=scores, x="proba", y=_name(metric), hue="model", style="model", markers=True, dashes=False)
     g.set(ylabel=f"{_display_name(metric)} score", xlabel="Probability of artificial UGC", xticks=[0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
     _, labels = g.get_legend_handles_labels()
     g.grid()
