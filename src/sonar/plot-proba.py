5840bac0d2731ae43b1d97b2f27b1f15f39909bb
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2024-09-13 14:02:19 +0200

    scripts to plot probas by lang

--- src/sonar/plot-proba.py
+++ src/sonar/plot-proba.py
@@ -6,49 +6,111 @@ import matplotlib.pyplot as plt
 from utils import (
     AVERAGE_ROCSMT_NORM_COLUMN,
     AVERAGE_ROCSMT_RAW_COLUMN,
-    METRIC_NAMES
+    METRIC_NAMES,
+    LANG_NAMES
 )
 
 COLORS = plt.cm.tab10.colors
 
+def plot_probas(
+        artificial_scores, 
+        multilingual_scores, 
+        metric,
+        models,
+        seeds, 
+        arti_score_column, 
+        norm_score_column, 
+        raw_score_column,
+        ax
+    ):
+    columns = ["model", "seed", "proba", "score"]
+    arti_data = artificial_scores[metric][columns[:3]]
+    arti_data["score"] = artificial_scores[metric][arti_score_column]
+    multi_data = pd.DataFrame(columns=columns)
+    multi_data["model"] = np.repeat(models, len(seeds))
+    multi_data["seed"] = np.tile(seeds, len(multilingual_scores[metric]))
+    multi_data["proba"] = np.repeat(0, len(multilingual_scores[metric])*len(seeds))
+    multi_data["score"] = np.repeat(multilingual_scores[metric][norm_score_column].values, len(seeds))
+    plot_data = pd.concat([arti_data, multi_data], ignore_index=True)
+    g = sns.lineplot(data=plot_data, x="proba", y="score", hue="model", style="model", markers=True, dashes=False, ax=ax)
+    _, labels = g.get_legend_handles_labels()
+    for k, model in enumerate(labels):
+        g.axhline(multilingual_scores[metric][raw_score_column].loc[model], ls="dashdot", c=COLORS[k])
+    g.grid()
+    
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--artificial-scores-file", help="path to artificial scores csv file", type=str)
-    parser.add_argument("--multilingual-scores-file", help="path to multilingual scores csv file", type=str)
+    parser.add_argument("-d", "--scores-dir", help="path to scores directory", type=str)
+    parser.add_argument("-m", "--metrics", help="list of metrics", type=str, nargs="+")
     args = parser.parse_args()
 
-    output_dir , file_name = os.path.split(args.artificial_scores_file)
-    metric = file_name.split("_")[0]
-    output_file = os.path.join(output_dir, f"{metric}_noise_proba_plot.pdf")
-
-    artificial_scores = pd.read_csv(args.artificial_scores_file)
-    multilingual_scores = pd.read_csv(args.multilingual_scores_file)
-
-    print("Concatenating multingual scores to artificial ones...")
+    artificial_scores = {}
+    multilingual_scores = {}
+    for metric in args.metrics:
+        artificial_scores[metric] = pd.read_csv(f"{args.scores_dir}/{metric}_artificial.csv")
+        multilingual_scores[metric] = pd.read_csv(f"{args.scores_dir}/{metric}_multilingual.csv")
     
     seeds = artificial_scores["seed"].unique()
+    probas = np.insert(artificial_scores["comet"]["proba"].unique(), 0, 0.0)
+    models = artificial_scores["comet"]["model"].unique()
+    target_langs = sorted(set([ col.split('__')[1].split('-')[1] for col in artificial_scores["comet"].columns if col.startswith("rocsmt")]))
 
-    arti_data = artificial_scores[["model", "seed", "proba", "avg"]]
-    multi_data = pd.DataFrame(columns=["model", "seed", "proba", "avg"])
-    multi_data["model"] = np.repeat(multilingual_scores["model"], len(seeds))
-    multi_data["seed"] = np.tile(seeds, len(multilingual_scores))
-    multi_data["proba"] = np.repeat(0, len(multilingual_scores)*len(seeds))
-    multi_data["avg"] = np.repeat(multilingual_scores[AVERAGE_ROCSMT_NORM_COLUMN], len(seeds))
-    plot_data = pd.concat([arti_data, multi_data], ignore_index=True)
+    for metric in multilingual_scores.keys():
+        multilingual_scores[metric].set_index("model", inplace=True)
+    
+    print("Plotting average scores...")
+    for metric in args.metrics:
+        output_file = f"{args.scores_dir}/{metric}_noise_proba_plot.pdf"
+        plt.clf()
+        fig, ax = plt.subplots()
+        g = plot_probas(
+            artificial_scores, 
+            multilingual_scores, 
+            metric,
+            models,
+            seeds,
+            arti_score_column="avg", 
+            norm_score_column=AVERAGE_ROCSMT_NORM_COLUMN, 
+            raw_score_column=AVERAGE_ROCSMT_RAW_COLUMN, 
+            ax=ax
+        )
+        g.set(ylabel=f"{METRIC_NAMES[metric]} score", xlabel="Probability of artificial UGC")
+        plt.tight_layout()
+        plt.savefig(output_file)
+    
+    print("Plotting language scores...")
+    output_file = f"{args.scores_dir}/noise_proba_plot_all.pdf"
+    fig, axes = plt.subplots(nrows=len(args.metrics), ncols=len(target_langs), sharex=True, sharey=True, figsize=(15, 6))
 
-    print("Creating plot...")
+    for i, metric in enumerate(args.metrics):
+        axes[i, 0].set_ylabel(f"{METRIC_NAMES[metric]} score")
+        for j, lang in enumerate(target_langs):
+            axes[0, j].set_title(r"English$\rightarrow$" + LANG_NAMES[lang])
+            plot_probas(
+                artificial_scores, 
+                multilingual_scores, 
+                metric,
+                models,
+                seeds,
+                arti_score_column=f"rocsmt_artificial__eng_Latn-{lang}__norm.en_mix_all.test", 
+                norm_score_column=f"rocsmt__eng_Latn-{lang}__norm.en.test", 
+                raw_score_column=f"rocsmt__eng_Latn-{lang}__raw.en.test", 
+                ax=axes[i, j]
+            )
 
-    multilingual_scores.set_index("model", inplace=True)
+    fig.supxlabel("Probability of artificial UGC")
 
-    g = sns.lineplot(data=plot_data, x="proba", y="avg", hue="model", style="model", markers=True, dashes=False)
-    g.set(ylabel=f"{METRIC_NAMES[metric]} score", xlabel="Probability of artificial UGC", xticks=[0, 0.1, 0.2, 0.3, 0.4, 0.5])
-    _, labels = g.get_legend_handles_labels()
-    for i, model in enumerate(labels):
-        g.axhline(multilingual_scores[AVERAGE_ROCSMT_RAW_COLUMN].loc[model], ls="dashdot", c=COLORS[i])
-    g.grid()
+    # Remove individual x-axis labels
+    for ax in axes.flat:
+        ax.set_xlabel('')
+
+    # Remove individual legends (except for the top right one)
+    for ax in axes[:,:-1].flat:
+        ax.legend_.remove()
+    axes[-1,-1].legend_.remove()
+    
     plt.tight_layout()
     plt.savefig(output_file)
-
-
 
 
