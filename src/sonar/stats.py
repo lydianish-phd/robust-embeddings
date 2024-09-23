697c48a91d259abb5a4037fc8aaafa2f98ca5555
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2024-09-23 11:26:47 +0200

    compute stats of model output files

---
+++ src/sonar/stats.py
@@ -0,0 +1,60 @@
+import os, argparse, json
+import sentencepiece as spm
+import numpy as np
+import re
+
+def find_usernames_hashtags_urls(text):
+    # Regular expression patterns
+    url_pattern = r'https?://[^\s]+'              # Matches URLs starting with http or https
+    username_pattern = r'@\w+'                    # Matches usernames starting with '@' and followed by word characters
+    hashtag_pattern = r'#\w+'                     # Matches hashtags starting with '#' and followed by word characters
+
+    return {
+        "urls": re.findall(url_pattern, text), 
+        "usernames": re.findall(username_pattern, text), 
+        "hashtags": re.findall(hashtag_pattern, text)
+    }
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input-files", type=str, required=True, help="Path to the input files", nargs="+")
+    parser.add_argument("-m", "--spm-model", type=str, required=True, help="Path to the sentencepiece model")
+    args = parser.parse_args()
+
+    sp = spm.SentencePieceProcessor(model_file=args.spm_model)
+    
+    for input_file in args.input_files:
+        output_dir, filename = os.path.split(args.input_file)
+        output_file = os.path.join(output_dir, f"stats.{filename}.json")
+
+        fertilities = []
+        type_token_ratios = []
+        urls = []
+        usernames = []
+        hashtags = []
+
+        print(f"Calculating stats for {args.input_file}...")
+
+        with open(args.input_file, "r") as f:
+            for line in f:
+                words = line.split()
+                tokens = sp.encode(line)
+                types = set(tokens)
+                fertilities.append(len(tokens) / len(words))
+                type_token_ratios.append(len(types) / len(tokens))
+                results = find_usernames_hashtags_urls(line)
+                urls.extend(results["urls"])
+                usernames.extend(results["usernames"])
+                hashtags.extend(results["hashtags"])
+        
+        stats = {
+            "fertility": round(np.array(fertilities).mean(), 2),
+            "TTR (%)": round(np.array(type_token_ratios).mean() * 100, 2),
+            "urls": len(urls),
+            "usernames": len(usernames),
+            "hashtags": len(hashtags)
+        }
+        print(stats)
+
+        with open(output_file, "w") as f:
+            json.dump(stats, f)
