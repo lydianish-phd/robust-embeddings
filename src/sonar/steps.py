b9b245c033dbae6f9ad95bd62c648636d58a41e7
NISHIMWE Lydia <lydia.nishimwe@inria.fr>
2024-10-16 15:41:07 +0200

    test number of total steps

--- src/sonar/steps.py
+++ src/sonar/steps.py
@@ -7,14 +7,11 @@ from datasets import (
 )
 
 seed = 42
-fraction_en_fr = 3
-fraction_fr = 3
-fraction_en = 2
 
 print("Loading datasets...")
 
-bilingual_data_dir = os.path.join(os.environ["DATASETS"], "rosonar/bilingual/concatenated")
-monolingual_data_dir = os.path.join(os.environ["DATASETS"], "rosonar/monolingual/concatenated")
+bilingual_data_dir = os.path.join(os.environ["DATASETS"], "rosonar/bilingual")
+monolingual_data_dir = os.path.join(os.environ["DATASETS"], "rosonar/monolingual")
 
 data_en_fr_files = {
     "train": f"{bilingual_data_dir}/eng-fra/train.eng_Latn-fra_Latn_chunks/train.eng_Latn-fra_Latn-*.jsonl"
@@ -27,16 +24,21 @@ data_fr_files = {
 data_fr = load_dataset("json", data_files=data_fr_files, streaming=True)
 
 data_en_files = {
-    "train": f"{monolingual_data_dir}/eng/train.eng_Latn-eng_Latn_chunks/train.eng_Latn-eng_Latn-*.jsonl"
+    "train": f"{monolingual_data_dir}/eng/part1/train.eng_Latn-eng_Latn_chunks/train.eng_Latn-eng_Latn-*.jsonl"
 }
 data_en = load_dataset("json", data_files=data_en_files, streaming=True)
 
-strategy = "all_exhausted"
-all_train_data = interleave_datasets([data_en_fr["train"], data_fr["train"], data_en["train"]], probabilities=[fraction_en_fr/8, fraction_fr/8, fraction_en/8], seed=seed, stopping_strategy=strategy)
+data_en_2_files = {
+    "train": f"{monolingual_data_dir}/eng/part2/train.eng_Latn-eng_Latn_chunks/train.eng_Latn-eng_Latn-*.jsonl"
+}
+data_en_2 = load_dataset("json", data_files=data_en_2_files, streaming=True)
+
+strategy = "first_exhausted"
+all_train_data = interleave_datasets([data_en_fr["train"], data_fr["train"], data_en["train"], data_en_2["train"]], probabilities=[4/8, 2/8, 1/8, 1/8], seed=seed, stopping_strategy=strategy)
 
 print("Interleaving strategy", strategy)
 
-data_loader = DataLoader(all_train_data, batch_size=32, num_workers=4)
+data_loader = DataLoader(all_train_data, batch_size=8, num_workers=40)
 
 # Initialize counters
 total_elements = 0
@@ -50,5 +52,4 @@ for batch in data_loader:
 
 # Print total counts
 print("Interleaving strategy", strategy)    
-print("Proportions (out of 8):", [fraction_en_fr, fraction_fr, fraction_en])    
 print(f"Total elements: {total_elements}")
